0b073a73a295fadda91add8d9200936cc03fd2e1
lavanya40 <lavanyaparikipandla@gmail.com>
2019-03-07 16:37:59 +0530

    hello

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBYiDfoJ3Wyl8LpfL9fauUx6mGIr0BlKQI",
+    authDomain: "lavanya-fda3c.firebaseapp.com",
+    databaseURL: "https://lavanya-fda3c.firebaseio.com",
+    projectId: "lavanya-fda3c",
+    storageBucket: "lavanya-fda3c.appspot.com",
+    messagingSenderId: "208217113088"
+  }
+};
+
